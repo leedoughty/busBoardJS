0e0e4465f72d630f027d3bbcc4141e63ee6cdcc4
Heather Lewis <heather.lewis@bbc.co.uk>
2020-07-09 11:37:26 +0100

    Refactors promises to use async await

--- index.js
+++ index.js
@@ -9,27 +9,26 @@ const constructPath = (stopCode) => {
   return `${BASE_URL}${service}${stopCode}/Arrivals?app_id=${apiCredentials.id}&app_key=${apiCredentials.key}`;
 };
 
-const getLiveTimes = (stopCode) => {
+const getLiveTimes = async (stopCode) => {
   const path = constructPath(stopCode);
   try {
-    return axios.get(path);
+    return await axios.get(path);
   } catch (error) {
     console.error("first block", error);
     return Promise.reject(error);
   }
 };
 
-const getBuses = (stopCode) => {
-  getLiveTimes(stopCode)
-    .then((response) => {
-      const fiveBuses = response.data.slice(0, 5);
+const getBuses = async (stopCode) => {
+  try {
+    const liveTimes = await getLiveTimes(stopCode);
+    const fiveBuses = liveTimes.data.slice(0, 5);
       fiveBuses.forEach((el) => {
         console.log(el.lineId, el.destinationName, el.timeToStation);
       });
-    })
-    .catch((error) => {
+  } catch (error) {
       console.log("second block", error);
-    });
+  }
 };
 
 getBuses(stopCodeInput);
